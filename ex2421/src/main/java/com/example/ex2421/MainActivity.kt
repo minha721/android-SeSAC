5bce9c1e848e8cbf79c6f0de9f61e6cbac39b538
minha721 <2minha721@gmail.com>
2022-02-01 15:54:53 +0900

    network programming

---
+++ ex2421/src/main/java/com/example/ex2421/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.ex2421
+
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val resultView = findViewById<TextView>(R.id.resultView)
+        resultView.text = isNetworkAvailable()
+    }
+
+    private fun isNetworkAvailable(): String {
+        val manager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw = manager.activeNetwork ?: return "offline"
+            val actNw = manager.getNetworkCapabilities(nw) ?: return "offline"
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    return "wifi online"
+                }
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    return "cellular online"
+                }
+                else -> "offline"
+            }
+        }
+        else {
+            if(manager.activeNetworkInfo?.isConnected ?: false){
+                return "online"
+            } else {
+                return "offline"
+            }
+        }
+    }
+}
